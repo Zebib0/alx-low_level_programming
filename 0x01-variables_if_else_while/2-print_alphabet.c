328de457bad24d5db3cccddb8a1815881df07e32
Zebib0 <gudetazebib@gmail.com>
2023-02-17 14:29:45 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Description - return a lower case character
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch;
+	char ch2;
+	 ch2 = tolower(ch);
+	 putchar(ch2);
+return (0);
+}
