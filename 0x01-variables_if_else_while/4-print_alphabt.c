8b5dcda2f89a21f2932800893fe4dbc7fdd08755
Zebib0 <gudetazebib@gmail.com>
2023-02-17 15:57:28 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - print all lowercase and uppercase alphabet
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z' && ch != 'q' && ch != 'e')
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
