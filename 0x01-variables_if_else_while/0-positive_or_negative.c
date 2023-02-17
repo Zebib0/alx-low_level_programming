437575be24da43abbf0d10950eb3f52b367f3566
Zebib0 <gudetazebib@gmail.com>
2023-02-17 12:03:09 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include<time.h>
 #include <stdio.h>
-/* The main function
- * it is the entery point 
- * Checks whether a number is zero, postive or negative
- * REturn 0 means successfully excecuted */
+/*
+ * The main function checks whether a number is zero, postive or negative
+ * It is the entery point
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
