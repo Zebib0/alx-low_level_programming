0bf5746ae1727dbc6d3f988461638f2a46606d06
Zebib0 <gudetazebib@gmail.com>
2023-02-18 05:58:54 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description - prints the size of datatypes
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = sizeof(int);
+	printf("%d", i);
+	return (0);
+}
