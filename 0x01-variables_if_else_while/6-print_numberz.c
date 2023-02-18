610d8fa115121815e4ede3c010688d2bb0722209
Zebib0 <gudetazebib@gmail.com>
2023-02-17 17:50:59 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+/**
+ * main - Entry point
+ * Description - print all single digit number greater than 0
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		putchar( i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
