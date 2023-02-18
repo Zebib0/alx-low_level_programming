04f4ab56c96277b3be9001ea745ca8a674f9c4f4
Zebib0 <gudetazebib@gmail.com>
2023-02-18 04:29:44 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
 
-	puts("\n");
 	puts(str);
 	return (0);
 }
