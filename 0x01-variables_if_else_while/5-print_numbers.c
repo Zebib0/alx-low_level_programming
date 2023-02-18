35ea218c7b46a56d203f88963d244168ff2ad6eb
Zebib0 <gudetazebib@gmail.com>
2023-02-17 16:56:07 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+/**
+ * main - Entry point
+ * Description - print all single digit number greater than 0
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	}
+
+	return (0);
+}
