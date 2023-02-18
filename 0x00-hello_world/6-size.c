7e5b2ade1f7cf7cb583906f122175f364b1cf387
Zebib0 <gudetazebib@gmail.com>
2023-02-18 06:32:29 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,26 @@
 
 /**
  * main - Entry point
- * Description - prints the size of datatypes
+ * Description - prints the size of datatypeis
  * Return: always 0 (success)
  */
 int main(void)
 {
 	int i;
+	int f;
+	int ll;
+	int l;
+	int ch;
 
 	i = sizeof(int);
-	printf("%d", i);
+	f = sizeof(float);
+	ll = sizeof(long long int);
+	l = sizeof(long int);
+	ch = sizeof(char);
+	printf("Size of a int: %d", i);
+	printf("Size of a float: %d", i);
+	printf("Size of a long long int: %d", i);
+	printf("Size of a long int: %d", i);
+	printf("Size of a char: %d", i);
 	return (0);
 }
