a8a63bb0c2f2589d99a5e39ed89bb93f31b61f07
Zebib0 <gudetazebib@gmail.com>
2023-02-18 06:46:59 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,7 +21,7 @@ int main(void)
 	printf("Size of a int: %d byte(s)\n", i);
 	printf("Size of a float: %d byte(s)\n", f);
 	printf("Size of a long long int: %d byte(s)\n", ll);
-	printf("Size of a long int: %d byte(s)\n", l);
+	printf("Size of a long int: %d byte(s) \n", l);
 	printf("Size of a char: %d byte(s)\n", ch);
 	return (0);
 }
