8c698caa2dd867735a9efb373900004fba32d96c
Zebib0 <gudetazebib@gmail.com>
2023-02-20 09:15:45 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,30 @@
  */
 int main(void)
 {
-        int a, b, c;
+	int a;
+	int b;
+	int c;
 
-        for (a = 0; a < 8; a++)
-        {
-                for (b = a + 1; b < 9; b++)
-                {
-                        for (c = b + 1; c < 10; c++)
-                        {
-                                if (a != b && a != c && b != c)
-                                {
-                                        putchar(a + '0');
-                                        putchar(b + '0');
-                                        putchar(c + '0');
-                                        if (a + b + c < 24)
-                                        {
-                                                putchar(',');
-                                                putchar(' ');
-                                        }
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (a = 0; a < 8; a++)
+	{
+		for (b = a + 1; b < 9; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				if (a != b && a != c && b != c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(b + '0');
+					if (a + b + c < 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
