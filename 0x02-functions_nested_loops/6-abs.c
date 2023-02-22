7fe2b9e8c1c364ec8119e94d90bcd6082a7fb5e7
Zebib0 <gudetazebib@gmail.com>
2023-02-22 15:42:22 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * _abs - Entry point
+ * Description: Absolute value of a number
+ * @c: the integer value
+ * Return: 1 if success, else
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
