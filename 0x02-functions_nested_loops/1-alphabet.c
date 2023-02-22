815b9da593d3cf4fcfc8b23371be0c3787e9552f
Zebib0 <gudetazebib@gmail.com>
2023-02-22 08:57:32 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include"main.h"
+
 /**
- * print_alphabet() - function to print alphabets
+ * print_alphabet - Entry point
  *
  * Description - The program displays all letters in lower case
- * Return : no return type
  */
 void print_alphabet(void)
 {
