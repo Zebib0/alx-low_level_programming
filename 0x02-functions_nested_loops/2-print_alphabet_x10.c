c376719228bdcc89a3cf2a03eaa0a0fa1ed2bd49
Zebib0 <gudetazebib@gmail.com>
2023-02-22 09:10:30 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,24 @@
-:
+#include"main.h"
+/**
+ * print_alphabet - function to print alphabets
+ * 
+ * Description - The program displays all letters in lower case
+ * Return : no return type 
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 0)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
