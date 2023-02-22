a0a4aa128370b57a0213b42d2cdae30c49c78163
Zebib0 <gudetazebib@gmail.com>
2023-02-22 08:52:12 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,4 @@
-#include "main.h"
-
-
-void print_alphabet(void);
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-        print_alphabet();
-        return (0);
-}
+#include"main.h"
 /**
  * print_alphabet() - function to print alphabets
  *
