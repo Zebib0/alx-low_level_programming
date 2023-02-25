83ce2bb829396583f18af92f2321b41b4a973bf3
Zebib0 <gudetazebib@gmail.com>
2023-02-24 16:09:05 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_numbers - display nunmber fron 0-9
+ *
+ *
+ * Return: no return value(void)
+ */
+void print_numbers(void)
+{
+	int i ='0';
+
+	while( i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
+
