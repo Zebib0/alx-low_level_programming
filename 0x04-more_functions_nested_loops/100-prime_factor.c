d3a21e923c760c765f39569eee1011e0c71a91b2
Zebib0 <gudetazebib@gmail.com>
2023-02-26 10:57:50 -0800

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,31 +1,14 @@
 # include <stdio.h>
 # include <math.h>
-void primeFactors(long int n);
 /**
  * main - Entry point
  * Description: displays the prime factors of a number
- * @n: the integer value
  * Return: 0 if success
  */
 
 int main(void)
 {
 	long int n = 612852475143;
-
-	primeFactors(n);
-	return (0);
-}
-/**
- * primeFactors - function for prime factors
- * Description: checks the existance of prime factor
- * and displays it.
- * @n: the integer value
- * @i: the value of prime factors
- * Return: no return value
- */
-
-void primeFactors(long int n)
-{
 	long int i;
 
 	while (n % 2 == 0)
@@ -33,7 +16,6 @@ void primeFactors(long int n)
 		printf("%d ", 2);
 		n = n / 2;
 	}
-
 	for (i = 3; i <= n; i = i + 2)
 	{
 		while (n % i == 0)
@@ -45,4 +27,5 @@ void primeFactors(long int n)
 	if (n > 2)
 		printf("%ld ", n);
 	putchar('\n');
+	return (0);
 }
