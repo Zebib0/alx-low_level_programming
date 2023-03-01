b6f68a79bee56131adc5051db1d566ff04e85197
Zebib0 <gudetazebib@gmail.com>
2023-03-01 19:27:30 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * reverse_array - reverses the position of interger
+ * of the array.
+ * @a: array of integers
+ * @n: the number of array members
+ *
+ * Return: no return value
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	for (i = 0; i >= n / 2; i--)
+	{
+		temp = a[n - i];
+		a[n - i] = a[i];
+		a[i] = temp;
+	}
+}
