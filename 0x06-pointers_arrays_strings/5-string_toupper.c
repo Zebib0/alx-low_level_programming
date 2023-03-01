e3c93186d31dc66cddeddf47366ef89ecb941fbd
Zebib0 <gudetazebib@gmail.com>
2023-03-01 20:38:44 +0000

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,18 @@
 #include"main.h"
 #include<ctype.h>
 
+/**
+ * string_toupper - converts lowercase letters to uppercase
+ * @str : string value
+ *
+ * Return: uppercase string
+ */
+
 char *string_toupper(char *str)
 {
 	char *s = str;
 
-	while (*s) 
+	while (*s)
 	{
 		*s = toupper((unsigned char) *s);
 		s++;
