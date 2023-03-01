d129b142c14cf84445246e5e8db957e1cb67d622
Zebib0 <gudetazebib@gmail.com>
2023-03-01 17:58:28 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,15 @@
 #include"main.h"
 #include<string.h>
-
+/**
+ * _strcat - concatinates two strings
+ *
+ * @dest : The first String
+ * @src : The String to be concatinated
+ * Return: String dest
+ */
 char *_strcat(char *dest, char *src)
 {
+	/** strcat : function to concatinate two strings */
 	strcat(dest, src);
-	return(dest);
+	return (dest);
 }
