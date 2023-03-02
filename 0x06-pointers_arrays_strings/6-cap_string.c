ea756fcd9d188dc2a06575196fc42693783f6b64
Zebib0 <gudetazebib@gmail.com>
2023-03-02 23:15:00 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,7 @@
 #include"main.h"
 #include<ctype.h>
 #include<string.h>
+#include<stdbool.h>
 
 
 /**
@@ -19,17 +20,21 @@ char *cap_string(char *str)
 	j = strlen(str);
 	for (i = 0; i < j; i++)
 	{
+		 bool exp = exp = (str[i] == 32) || (str[i] == 44) || (str[i] == 40) ||
+                (str[i] == 41) || (str[i] == 46) ||
+                (str[i] == 33) || (str[i] == 63) || (str[i] == 59) ||
+                (str[i] == 123) || (str[i] == 125) || (str[i] == 34);
+
 		if (str[i] == '\n' || str[i] == '\t')
 		{
-			i++;
-			str[i] = toupper(str[i]);
+			str[i + 1]= toupper(str[i + 1]);
 		}
-		else if (str[i] == ' ' || str[i] == ',' || str[i] == ';' ||
-				str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"'
-				|| str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		else if (exp)
 		{
-			i++;
-			str[i] = toupper(str[i]);
+			 str[i + 1] = toupper(str[i + 1]);
+		}
+		else
+		{
 		}
 	}
 	return (str);
