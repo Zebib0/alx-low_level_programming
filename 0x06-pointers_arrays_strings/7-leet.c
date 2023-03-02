3ad8db1c1f9d422068ab64acde98f947a26a1fa7
Zebib0 <gudetazebib@gmail.com>
2023-03-02 23:55:05 +0000

    tsk

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,33 +1,33 @@
-#include"main.h"
-#include<string.h>
-
-char *leet(char *str)
+#include "main.h"
+/**
+ * leet -  encodes a string into 1337.
+ * @s: string to code
+ * Return: the enconde string.
+ */
+char *leet(char *s)
 {
-	int i, n;
+	int i;
+	int j;
+	int k;
 
-	n = strlen(str);
-	for(i = 0; i < n; i++)
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if ( str[i] == 'a' || str[i] == 'A')
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
 		{
-			str[i] = '4';
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = e[k];
+			}
+			j++;
 		}
-		else if ( str[i] == 'e' || str[i] == 'E')
-                {
-                        str[i] = '3';
-                }
-		else if ( str[i] == 'o' || str[i] == 'O')
-                {
-                        str[i] = '0';
-                }
-		else if ( str[i] == 't' || str[i] == 'T')
-                {
-                        str[i] = '7';
-                }
-		else if( str[i] == 'l' || str[i] == 'L')
-                {
-                        str[i] = '1';
-                }
+		i++;
 	}
-	return (str);
+	return (s);
 }
