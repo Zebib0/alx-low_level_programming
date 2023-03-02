43a394861bd3e04f14db1c1d25e91e8a5f37e14d
Zebib0 <gudetazebib@gmail.com>
2023-03-02 23:24:01 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,18 +20,17 @@ char *cap_string(char *str)
 	j = strlen(str);
 	for (i = 0; i < j; i++)
 	{
-		 bool exp = exp = (str[i] == 32) || (str[i] == 44) || (str[i] == 40) ||
-                (str[i] == 41) || (str[i] == 46) ||
-                (str[i] == 33) || (str[i] == 63) || (str[i] == 59) ||
-                (str[i] == 123) || (str[i] == 125) || (str[i] == 34);
-
+		bool exp = exp = (str[i] == 32) || (str[i] == 44) || (str[i] == 40) ||
+			 (str[i] == 41) || (str[i] == 46) ||
+			 (str[i] == 33) || (str[i] == 63) || (str[i] == 59) ||
+			 (str[i] == 123) || (str[i] == 125) || (str[i] == 34);
 		if (str[i] == '\n' || str[i] == '\t')
 		{
-			str[i + 1]= toupper(str[i + 1]);
+			str[i + 1] = toupper(str[i + 1]);
 		}
 		else if (exp)
 		{
-			 str[i + 1] = toupper(str[i + 1]);
+			str[i + 1] = toupper(str[i + 1]);
 		}
 		else
 		{
