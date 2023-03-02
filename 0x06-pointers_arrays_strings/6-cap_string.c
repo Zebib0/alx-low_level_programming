0e7f2bfa200685610199dd54b17bce0e293c45c6
Zebib0 <gudetazebib@gmail.com>
2023-03-02 23:52:08 +0000

    tsk

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,7 @@
 
 /**
  * cap_string - converts lowercase letters to uppercase
- * @str : string value
+ * @s : string value
  *
  * Return: return string with uppercase with uppercase
  * first letter.
@@ -14,24 +14,24 @@
 
 char *cap_string(char *s)
 {
-        int j, i;
-        bool exp;
+	int j, i;
+	bool exp;
 
-        j= strlen(s);
-        if (s[0] >= 97 && s[0] <= 122)
-                s[0] = s[0] - 32;
-        for (i = 0; i < j; i++)
-        {
-                exp = (s[i] == 32) || (s[i] == 44) || (s[i] == 40) ||
-                (s[i] == 41) || (s[i] == 46) ||
-                (s[i] == 33) || (s[i] == 63) || (s[i] == 59) ||
-                (s[i] == 123) || (s[i] == 125) || (s[i] == 34);
-                if (exp || s[i] == '\n' || s[i] == '\t')
-                {
-                        if (s[i + 1] >= 97 && s[i + 1] <= 122)
-                                s[i + 1] = s[i + 1] - 32;
-                }
-        }
-        return (s);
+	j = strlen(s);
+	if (s[0] >= 97 && s[0] <= 122)
+		s[0] = s[0] - 32;
+	for (i = 0; i < j; i++)
+	{
+		exp = (s[i] == 32) || (s[i] == 44) || (s[i] == 40) ||
+			(s[i] == 41) || (s[i] == 46) ||
+			(s[i] == 33) || (s[i] == 63) || (s[i] == 59) ||
+			(s[i] == 123) || (s[i] == 125) || (s[i] == 34);
+		if (exp || s[i] == '\n' || s[i] == '\t')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
+		}
+	}
+	return (s);
 }
 
