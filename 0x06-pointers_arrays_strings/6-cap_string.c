992730cbfa8780f0d546950ef1c2eebcdbce9896
Zebib0 <gudetazebib@gmail.com>
2023-03-02 18:37:55 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,33 +2,43 @@
 #include<ctype.h>
 #include<string.h>
 
+
+/**
+ * cap_string - converts lowercase letters to uppercase
+ * @str : string value
+ *
+ * Return: return string with uppercase with uppercase
+ * first letter.
+ */
+
 char *cap_string(char *str)
 {
 	int i;
-        int j;
+	int j;
 
-        j = strlen(str);
-        for (i = 0; i < j; i++)
+	j = strlen(str);
+	for (i = 0; i < j; i++)
 	{
-                if (i == 0)
-		str[0] = toupper(str[i]);
+		if (i == 0)
+			str[0] = toupper(str[i]);
 		{
-		int space;
-		for(space = 0; space < j; space++)
-		{
-		if (str[i] == ' ')
+			int space;
+
+			for (space = 0; space < j; space++)
+			{
+				if (str[i] == ' ')
+				{
+					i++;
+					str[i] = toupper(str[i]);
+				}
+				space++;
+			}
+		}
+		if (str[i] == '\n')
 		{
 			i++;
 			str[i] = toupper(str[i]);
 		}
-		space++;
-		}
-		}
-		if (str[i] == '\n')
-			 {
-                        i++;
-                        str[i] = toupper(str[i]);
-			 }
 	}
-        return (str);
+	return (str);
 }
