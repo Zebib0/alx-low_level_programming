8956bbb6054bcfbee39e7e442be2ac3138cb2705
Zebib0 <gudetazebib@gmail.com>
2023-03-02 21:21:03 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,22 +19,14 @@ char *cap_string(char *str)
 	j = strlen(str);
 	for (i = 0; i < j; i++)
 	{
-		if (i == 0)
-			str[0] = toupper(str[i]);
+		if (str[i] == '\n' || str[i] == '\t')
 		{
-			int space;
-
-			for (space = 0; space < j; space++)
-			{
-				if (str[i] == ' ')
-				{
-					i++;
-					str[i] = toupper(str[i]);
-				}
-				space++;
-			}
+			i++;
+			str[i] = toupper(str[i]);
 		}
-		if (str[i] == '\n')
+		else if (str[i] == ' ' || str[i] == ',' || str[i] == ';' ||
+				str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"'
+				|| str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
 			i++;
 			str[i] = toupper(str[i]);
