a6d370856b08c6914db8d67d2ffec1c58a49258e
Zebib0 <gudetazebib@gmail.com>
2023-03-06 11:36:22 +0000

    new task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,21 @@
 #include"main.h"
 #include<string.h>
 
+/**
+ * _strspn - prints the position of a charachter which is not
+ * a member of the second string value.
+ * @s : string value
+ * @accept: string value
+ *
+ *
+ * Return: pointer which contains
+ *i value after the desired char
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	char l;
+
 	l = strspn(s, accept);
 	return (l);
 }
