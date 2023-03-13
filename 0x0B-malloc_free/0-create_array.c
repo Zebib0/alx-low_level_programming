c550c125a7b7528efcad2bacd419e94a7b27bd20
Zebib0 <gudetazebib@gmail.com>
2023-03-13 19:52:16 +0000

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,17 +10,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-        char *str;
-        unsigned int i;
+	char *str;
+	unsigned int i;
 
-        if (size == 0)
-        {
-                return (NULL);
-        }
-        str = malloc(sizeof(char) * size);
-        for (i = 0; i < size; i++)
-        {
-                str[i] = c;
-        }
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	str = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
 	return (str);
 }
