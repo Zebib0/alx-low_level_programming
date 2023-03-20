dba7c37335e84c712dd96def1378d5fadfd258dd
Zebib0 <gudetazebib@gmail.com>
2023-03-20 22:13:12 +0000

    Prepreocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    printf(__FILE__);
+    putchar('\n');
+    return 0;
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf(__FILE__);
+	putchar('\n');
+    return 0;
+}
