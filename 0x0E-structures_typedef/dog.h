0ad0876c07658d5479e1ec33b011f4acae41b77d
Zebib0 <gudetazebib@gmail.com>
2023-03-21 00:30:59 +0000

    Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * struct dog - made up of variables
+ * that identifies a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: includes information about
+ * the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
