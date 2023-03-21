11043ada1bb01facfebcc0c33b86aea568dfb037
Zebib0 <gudetazebib@gmail.com>
2023-03-21 01:09:24 +0000

    Preprocessing

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include"dog.h"
-
+/**
+ * init_dog - intialize any structures given
+ * @d: a struct for dog
+ * @name: a string for the name of the dog
+ * @age: a age of the dog
+ * @owner: the name of the owner
+ * Return: nothing
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	(*d).name = name;
