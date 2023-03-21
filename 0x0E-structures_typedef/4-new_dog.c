5b07b74a2ab60b13640d443948904b6bf77ec938
Zebib0 <gudetazebib@gmail.com>
2023-03-21 19:32:04 +0000

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,7 +26,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i <= size1; i++)
-		(*dog1).name = name;
+		(*dog1).name[i] = name[i];
 	(*dog1).age = age;
 	for (size2 = 0; owner[size2]; size2++)
 		size2++;
