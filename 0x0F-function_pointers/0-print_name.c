8eb36f2b212526f525e4170ad466f7c748cd729b
Zebib0 <gudetazebib@gmail.com>
2023-03-22 21:34:57 +0000

    fun ptr

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,10 @@
  * @name: the string parameter
  * @f: pointer to function that takes a char * as a parameter and returns void
  *
- * Return: nothing (void)
+ * Return: no return value
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name || f)
+	if (name && f)
 	f(name);
 }
