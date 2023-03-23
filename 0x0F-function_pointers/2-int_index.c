8f3f0e5b0ae0c96b805692e02cd7732792640624
Zebib0 <gudetazebib@gmail.com>
2023-03-23 20:44:22 +0000

    fun ptr

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include"function_pointers.h"
+/**
+ * int_index - Function that searches for a given integer
+ * @array: integer array
+ * @size: Size of the array
+ * @cmp: Pointer to a function
+ * Return: The index of the array or -1 if it fails
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
