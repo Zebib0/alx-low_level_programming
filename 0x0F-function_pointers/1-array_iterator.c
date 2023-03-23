ef179bf02f0ef1c7344eb4d206317c76de0b2c4f
Zebib0 <gudetazebib@gmail.com>
2023-03-23 19:02:27 +0000

    Fun ptr

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include"function_pointers.h"
+/**
+ * array_iterator - a function iterates through
+ * array and execute a callback func each time
+ * @array: arrays
+ * @size: the size of the array
+ * @action: a callback function we need to execute each time
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || size <= 0)
+		return;
+	for (i = 0; i < size; i++)
+	(*action)(array[i]);
+}
