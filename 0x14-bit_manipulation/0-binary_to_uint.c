ecde7a46f549d29871eded3bd77404838e83d03f
Zebib0 <gudetazebib@gmail.com>
2023-04-06 23:26:06 +0000

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,26 @@
 #include"main.h"
 #include<string.h>
-#include<math.h>
 /**
  * binary_to_uint - converts binary to unsigned interger
- * @b: String of binary digits to be converted 
+ * @b: String of binary digits to be converted
  *
  * Return: integers
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimalnum = 0, p = 0;
+	unsigned int decimalnum = 0, p = 1;
 	int size = 0, i;
 
+	if (!b)
+		return (0);
 	size = strlen(b);
 	for (i = size - 1; i >= 0; i--)
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 		if (b[i] == '1')
-			decimalnum = decimalnum + pow(2, p);
-		p++;
+			decimalnum = decimalnum + p;
+		p *= 2;
 	}
 	return (decimalnum);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
 unsigned int binary_to_uint(const char *b);
