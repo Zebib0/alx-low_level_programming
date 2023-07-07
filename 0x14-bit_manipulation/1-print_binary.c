62cc440e85100ab95d68c12ef492ed5473d9ce69
Zebib0 <gudetazebib@gmail.com>
2023-07-07 17:28:28 +0000

    bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * binary_to_uint - a function that converts a binary number to decimal
+ * print_binary - a function that converts a binary number to decimal
  * @n: a decimal value
  * Return: null
  */
@@ -9,12 +9,14 @@ void print_binary(unsigned long int n)
 	int i, zero = 1;
 	unsigned long int m;
 	int f;
+
 	f = sizeof(unsigned long int) * 8 - 1;
 
 	for (i = f; i >= 0; i--)
 	{
 		m = 1UL << i;
-		if (n & m){
+		if (n & m)
+		{
 			_putchar('1');
 			zero = 0;
 		}
