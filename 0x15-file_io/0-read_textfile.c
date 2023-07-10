a493cc83acd3c482a90ee1061286f3fe0fb48b0f
Zebib0 <gudetazebib@gmail.com>
2023-07-10 21:17:10 +0000

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * read_textfile - reads a text file and prints it
  * to the POSIX standard output
@@ -9,39 +8,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *f = fopen(filename, "r");
-	char *buff;
-	ssize_t bytes_read, byte_written;
+	int fd;
+	size_t byte_read, byte_written;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-	if (f == NULL)
+	buf = malloc(letters);
+	if (buf == NULL)
 		return (0);
-	/**
-	 * Malloc creates memory to hold
-	 * the text read from the file.
-	 */
-	buff = malloc(sizeof(char) * (letters + 1));
-	if (buff == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 	{
-		fclose(f);
+		free(buf);
 		return (0);
 	}
-	/**
-	 * function fread() is used to read the file
-	 * and put it on the space prepared before(buff)
-	 * byte_read counts the number of letters that are being read.
-	 */
-	bytes_read = fread(buff, 1, letters, f);
-	buff[bytes_read] = '\0';
-	byte_written = fwrite(buff, 1, bytes_read, stdout);
-	if (byte_written != bytes_read)
+	byte_read = read(fd, buf, letters);
+	if (byte_read != letters)
 	{
-		fclose(f);
-		free(buff);
+		free(buf);
+		close(fd);
 		return (0);
 	}
-	fclose(f);
-	free(buff);
-	return (bytes_read);
+	byte_written = write(STDOUT_FILENO, buf, byte_read);
+	free(buf);
+	close(fd);
+	return ((byte_written == byte_read) ? byte_read : 0);
 }
