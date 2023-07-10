6e62abcbad34ebb6a14d29244ffc0f5a713c9ea6
Zebib0 <gudetazebib@gmail.com>
2023-07-10 21:44:42 +0000

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,7 @@
  * @filename: the name of a file to create
  * @text_content: the content of the file
  * Return: 1 on success -1 on failure
+ */
 
 int create_file(const char *filename, char *text_content)
 {
