13fedf5de9c385c0bb1c0e1afc422d881601e74e
Zebib0 <gudetazebib@gmail.com>
2023-08-17 22:00:28 +0000

    task 4 fix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,7 +7,12 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
-
+	
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+	}
 	if (head == NULL)
 	{
 		while ((temp = head) != NULL)
@@ -15,10 +20,5 @@ void free_dlistint(dlistint_t *head)
 			head = head->next;
 			free(temp);
 		}
-	}
-	else
-	{
-		while (head->prev != NULL)
-			head = head->prev;
 	}
 }
