54bca1068768e2fe09c73203b8a4ff764a8aaaf5
Zebib0 <gudetazebib@gmail.com>
2023-08-17 20:16:08 +0000

    betty fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
  * print_dlistint - print all the element of
  * a doubly linked list
  * @h: head of the list
- * return: the number of nodes
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
