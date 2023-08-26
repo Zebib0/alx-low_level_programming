cb6c4a0cab8f4f3310cb7da64f3d71d50f877b52
Zebib0 <gudetazebib@gmail.com>
2023-08-26 02:33:22 +0000

    last

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Zebib0/alx-low_level_programming/blob/master/0x18-dynamic_libraries/0x18.c/libfunction2.so
+wget -P /tmp
 export LD_PRELOAD=/tem/libfunction2.so
